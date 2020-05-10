6769422324c73ee20085ff334d686ac253a1f6f0
Youngjun <56021593+Fintecuriosity11@users.noreply.github.com>
2020-05-11 01:28:09 +0900

    Create 03_assignment_statement.jl

---
+++ Think Julia/03_assignment_statement.jl
@@ -0,0 +1,12 @@
+# 할당문 (Assignment statement)
+# 새로운 변수를 만들고, 그 변수에 값을 할당함.
+
+message = "And now for something completely different"        # 문자열(String)을 message라는 변수에 할당  
+
+n = 17                                                        # 정수(integer) 17을 n에 할당
+
+\pi_val = 3.141592653589793                                   # 원주율 𝞹의 값을 𝞹_val에 할당
+
+# REPL에서는 \pi라고 치고 Tab을 누르면 π가 입력된다.
+
+# 참고로 주석에 다양한 𝛑 부호를 넣고 싶으면 https://fsymbols.com/signs/pi/ 에 접속하여 부호를 붙여넣을 수 있다.
