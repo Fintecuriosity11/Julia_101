f4f4846c5734adc503f3073e5e449dca679b0eae
Youngjun <56021593+Fintecuriosity11@users.noreply.github.com>
2020-05-24 01:49:18 +0900

    Create 01_boolean_expression.jl

---
+++ Think Julia/ch05_condition_recursive/01_boolean_expression.jl
@@ -0,0 +1,21 @@
+# 논리 표현식(Boolean expression)은 평가 결과가 true이거나 false인 표현식이다.
+# 아래의 예제에서는 == 연산자를 사용하는데, 이는 두 연산자를 비교해서 같으면 true, 거짓이면 false를 반환하는 연산자
+
+5==5
+true
+
+5==6
+false
+
+# true와 false는 문자열이 아니고, Bool 자료형에 속하는 특별한 값이다. 
+
+typeof(false)
+Bool
+
+x != y           # x와 y는 같지 않다. 
+x ≠ y            # (\ne 탭)
+x > y            # x가 y보다 크다
+x < y            # x가 y보다 작다
+x ≥ y            # x가 y보다 크거나 같다
+x ≤ y            # (\le 탭)
+x <= y           # x가 y보다 작거나 같다
